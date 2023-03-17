4db6bf054c139724657000e118c2cc33cfdbbc57
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2023-03-16 22:07:27 -0600

    Remove background scrolling when modal is open

--- js/my_portfolio.js
+++ js/my_portfolio.js
@@ -1,32 +1,32 @@
 function openNav() {
-  document.getElementById("myNav").style.width = "100%";
+  document.getElementById('myNav').style.width = '100%';
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.width = "0%";
+  document.getElementById('myNav').style.width = '0%';
 }
 
 var prevScrollpos = window.pageYOffset;
-var navbarMobile = document.querySelector(".navbar")
-var navbarWrapper = document.querySelector(".nav-wrapper")
-const desktopViewport = window.matchMedia('(min-width: 768px)')
+var navbarMobile = document.querySelector('.navbar');
+var navbarWrapper = document.querySelector('.nav-wrapper');
+const desktopViewport = window.matchMedia('(min-width: 768px)');
 
 function navbarEffect() {
-  var nav = desktopViewport.matches? navbarWrapper: navbarMobile;
+  var nav = desktopViewport.matches ? navbarWrapper : navbarMobile;
   var currentScrollPos = window.pageYOffset;
 
   if (prevScrollpos > currentScrollPos) {
-    nav.style.top = "0";
-    nav.classList.add("nav-wrapper-scroll-class");
+    nav.style.top = '0';
+    nav.classList.add('nav-wrapper-scroll-class');
     if (window.scrollY == 0) {
-      nav.classList.remove("nav-wrapper-scroll-class");
-     }
+      nav.classList.remove('nav-wrapper-scroll-class');
+    }
   } else {
-    nav.style.top = `-${nav.getBoundingClientRect().height}px`
-    nav.classList.remove("nav-wrapper-scroll-class");
+    nav.style.top = `-${nav.getBoundingClientRect().height}px`;
+    nav.classList.remove('nav-wrapper-scroll-class');
     if (window.scrollY == 0) {
-      nav.style.top = "0";
-     }
+      nav.style.top = '0';
+    }
   }
   prevScrollpos = currentScrollPos;
 }
